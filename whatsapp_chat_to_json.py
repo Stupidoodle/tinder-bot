8d8d1e9164b59c23f5f3b0df00502038d441510d
Bryan <bryan@punknown.net>
2023-05-12 01:12:53 +0200

    Test selenium

---
+++ whatsapp_chat_to_json.py
@@ -0,0 +1,17 @@
+import json
+
+conversation = ""
+
+messages = []
+lines = conversation.strip.split("\n")
+
+for line in lines:
+    parts = line.split("] ")
+    if len(parts) == 2:
+        role, content = parts[1].split(": ", 1)
+        messages.append({"role": role.strip(), "content": content.strip()})
+
+json_data = json.dumps(messages, indent=4)
+
+with open("conversation.json", "w") as file:
+    file.write(json_data)
